f2dd526785e770908ff4d7ae025febac58231c15
Thomas Chamelot <chamelot.thomas@gmail.com>
2024-12-20 19:06:13 +0100

    feat: base simulation lib

---
+++ src/trr_sim/__init__.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+import numpy as np
+import numpy.typing as npt
+
+DATA = Path(__file__).parent / "track"
+TRACK = DATA / "robot.urdf"
+POLYLINE = np.load(DATA / "track.npy")
+
+
+def load_track(client):
+    """Load the TRR track into pybullet"""
+    return client.loadURDF(str(TRACK), [0, 0, 0])
+
+
+def get_waypoints(position: npt.ArrayLike = [0, 0], yaw: float = 0.0):
+    """Return the waypoints representing the track
+
+    Passing option position and yaw will modify the waypoints so that the provided position becomes the origin of the referential.
+    """
+    translation = np.array(position)
+    c = np.cos(yaw)
+    s = np.sin(yaw)
+    rotation = np.array([[c, -s], [s, c]])
+    return (POLYLINE - translation) @ rotation
